c4d1cd81c0f624a990e1d0f3d8c2f59db3c565dd
Brandon Hubbard <bkhubbard@gmail.com>
2016-08-23 14:45:41 -0700

    Setting up project

---
+++ wp-harvest-api.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* WP Harvest API
+*
+* @package WP-Harvest-API
+*/
+
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+/**
+ * Harvest API Class.
+ */
+class HarvestAPI {
+
+
+
+}
