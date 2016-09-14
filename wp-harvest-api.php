95d7c411f552555241db9cae0ddd812b2fbbddf5
Brandon Hubbard <bkhubbard@gmail.com>
2016-09-13 18:44:34 -0700

    starting functions

--- wp-harvest-api.php
+++ wp-harvest-api.php
@@ -5,16 +5,45 @@
 * @package WP-Harvest-API
 */
 
-/* Exit if accessed directly */
+/* Exit if accessed directly. */
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-
+/* Check if class exists. */
 if( ! class_exists( 'HarvestAPI' ) ) {
 
 	/**
 	* Harvest API Class.
  	*/
  	class HarvestAPI {
+
+
+   	public function get_all_clients() {
+
+   	}
+
+   	public function get_client() {
+
+   	}
+
+   	public function create_client() {
+
+   	}
+
+   	public function update_client() {
+
+   	}
+
+   	public function delete_client() {
+
+   	}
+
+   	public function get_all_contacts() {
+
+   	}
+
+   	public function get_all_contacts_for_client() {
+
+   	}
 
 
 
